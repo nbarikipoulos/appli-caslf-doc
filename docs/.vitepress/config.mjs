e167e3d3ab1e82dd5e345e4bf476adc63d0ac309
nbarikipoulos <nicolas.barriquand@outlook.fr>
2025-01-04 22:56:22 +0100

    Updates

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,56 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: 'CASLF',
+  description: 'Documentation de l\'application CASLF',
+  lang: 'fr-FR',
+  base: '/appli-caslf-doc/',
+  cleanUrls: true,
+  themeConfig: {
+    logo: '/favicon.png',
+    nav: [
+      { text: 'Accueil', link: '/' },
+      { text: 'Installation',
+        items: [
+          {text: 'Généralités', link: '/install/intro'},
+          {text: 'Android', link: '/install/android/install'},
+          {text: 'IPhone', link: '/install/iphone/connect'}
+        ]
+      },
+      { text: 'Guide d\'utilisation', link: '/user_guide/intro' },
+    ],
+
+    sidebar: [
+      {
+        text: 'Installation',
+        items: [
+          { text: 'Généralités', link: '/install/intro' },
+          { text: 'Android',
+            items: [
+              {text: 'installer l\'appli', link: '/install/android/install'},
+              {text: 'première connexion', link: '/install/android/connect'},
+              {text: 'l\'appli est prête!', link: '/install/android/end'}
+            ]
+          },
+          { text: 'IPhone', 
+            items: [
+              {text: 'installer l\'appli', link: '/install/iphone/install'},
+              {text: 'première connexion', link: '/install/iphone/connect'},
+              {text: 'activer les notifications', link: '/install/iphone/notification'},
+              {text: 'l\'appli est prête!', link: '/install/iphone/end'}
+            ]
+          }
+        ],
+      },
+      {
+        text: 'Guide d\'utilisation',
+        items: [
+          { text: 'Présentation', link: '/user_guide/intro' }
+        ]
+      }
+    ],
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/nbarikipoulos/appli-caslf-doc' }
+    ]
+  }
+})
